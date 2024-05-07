379135d4894b94545fb66f266cf23e29f07e6416
Mandinha11 <169157997+Mandinha11-8@users.noreply.github.com>
2024-05-07 10:46:39 -0300

    Pratica-ANP

---
+++ Pratica-ANP/Pessoa.java
@@ -0,0 +1,23 @@
+package ifsc;
+
+public class Pessoa {
+	
+	public String nome;
+	public String CPF;
+	
+	public String getCPF() {
+		return CPF;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setCPF(String CPF) {
+		this.CPF = CPF;
+	}
+}
